bb29a12635cfdf48e976df99faeceeec4f22a65f
Aryan Sachan <sachanaryaniit@gmail.com>
2025-10-29 08:27:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from  "express";
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my Express Server");
+})
+
+app.get("/about", (req, res) => {
+    res.send("<h1>Myself Aryan Sachan</h1>")
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
