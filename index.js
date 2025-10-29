c8f4b990485a69a28b76e578dc209ca57ec39ed8
Aryan Sachan <sachanaryaniit@gmail.com>
2025-10-29 09:05:19 +0530

    modifying index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get("/about", (req, res) => {
     res.send("<h1>Myself Aryan Sachan</h1>")
 })
 
+const data = {
+    name: "Aryan Sachan",
+    age: 20,
+    college: "VIT Chennai"  
+}
+
+app.get("/data", (req, res) => {
+    res.json(data);
+})
+
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
